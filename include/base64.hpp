50c120bcd7d0b79c2246f7f12542bcf87f0f5f71
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2022-02-08 20:57:29 +0200

    Init files

---
+++ include/base64.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+void func()
+{
+    std::cout << "Hello world!" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "base64.hpp"
+
+int main()
+{
+    func();
+
+    /* Do not write code */
+    system("pause > nul");
+    return EXIT_SUCCESS;
+}
