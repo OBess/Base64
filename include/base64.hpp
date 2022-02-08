db34dc40d66128fb9836393f75170d50a2e86b42
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2022-02-08 22:07:12 +0200

    add encoding impl

--- include/base64.hpp
+++ include/base64.hpp
@@ -6,29 +6,43 @@ namespace base64
 {
     static constexpr std::string_view base64Chars{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="};
 
-    template <class T>
-    constexpr std::string encode(T data)
+    // ENCODING
+    constexpr std::string encode(const char *data, const size_t size)
     {
-        constexpr auto size = sizeof(data);
-        const char *s_data = reinterpret_cast<const char *>(data);
-
         std::string encoded;
         encoded.reserve(size * 2);
 
-        for (size_t i = 0; i < size; ++i)
+        for (size_t i = 0; i < size; i += 3)
         {
-            const char a = s_data[i++];
-            const char b = i < size ? s_data[i++] : 64;
-            const char c = i < size ? s_data[i++] : 64;
+            const char a = data[i];
+            const char b = i + 1 < size ? data[i + 1] : 0;
+            const char c = i + 2 < size ? data[i + 2] : 0;
 
-            encoded.push_back(a >> 2);
-            encoded.push_back((a & 3) << 4 | b >> 4);
-            encoded.push_back((b & 15) << 4 | c >> 6);
-            encoded.push_back(c & 63);
+            encoded.push_back(base64Chars[a >> 2]);
+            encoded.push_back(base64Chars[(a & 3) << 4 | b >> 4]);
+            encoded.push_back(base64Chars[i + 1 < size ? (b & 15) << 2 | c >> 6 : 64]);
+            encoded.push_back(base64Chars[i + 2 < size ? c & 63 : 64]);
         }
 
         return encoded;
     }
+
+    constexpr std::string encode(std::string_view data)
+    {
+        return encode(data.data(), data.size());
+    }
+
+    constexpr std::string encode(const std::string &data)
+    {
+        return encode(data.data(), data.size());
+    }
+
+    template <class T>
+    constexpr std::string encode(T data)
+    {
+        return encode(reinterpret_cast<char *>(&data), sizeof(data));
+    }
+    // ~ENCODING
 
     template <class T>
     constexpr T decode(std::string_view data)
--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
 #include "base64.hpp"
+#include "fmt.hpp"
 
 int main()
 {
-    
+    std::string_view data{"demon"};
+    auto result = base64::encode(data);
+
+    fmt::println("\"{}\" in base64 is \"{}\"", data, result);
 
     /* Do not write code */
     return EXIT_SUCCESS;
